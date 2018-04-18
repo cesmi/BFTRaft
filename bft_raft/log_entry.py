0334465a33d1090fe76a67e6fb721e1e95981fd2
Christian Smith <cesm@umich.edu>
2018-04-18 14:29:18 -0400

    added a comment

--- bft_raft/log_entry.py
+++ bft_raft/log_entry.py
@@ -7,5 +7,7 @@ class LogEntry(object):
         self.operation = operation
         self.client_id = client_id
 
-    def incremental_hash(self):
+    def incremental_hash(self) -> bytes:
+        '''Calculates the hash of the previous incremental hash appended to
+        this log entry.'''
         pass  # TODO
