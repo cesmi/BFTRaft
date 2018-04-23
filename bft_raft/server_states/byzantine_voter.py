c442d51ea4328ab6aa556d99f5f48e463b3302bb
Steven Schulte <spschul@umich.edu>
2018-04-22 20:28:56 -0400

    Byzantine attempts to sabotage elections

--- bft_raft/server_states/byzantine_voter.py
+++ bft_raft/server_states/byzantine_voter.py
@@ -5,11 +5,12 @@ from ..servers.base import BaseServer
 from .state import State
 
 from .voter import Voter
+from .byzantine_candidate import ByzantineCandidate0
 
 class ByzantineVoter0(Voter):
     @staticmethod
     def construct(uncorrupted):
-        return ByzantineVoter0(uncorrupted.term, uncorrupted.server, uncorrupted)
+        return ByzantineCandidate0(uncorrupted.term, {}, uncorrupted.server, uncorrupted)
 
     def __init__(self, term: int,
                  server: BaseServer,
